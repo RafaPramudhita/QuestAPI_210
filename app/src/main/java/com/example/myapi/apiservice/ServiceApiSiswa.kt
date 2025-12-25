2c3940230b818fa56a9ccecb86144aa14c0b8cab
RafaPramudhita <rafapram360@gmail.com>
2025-12-25 11:43:33 +0700

    Create ServiceApiSiswa interface

---
+++ app/src/main/java/com/example/myapi/apiservice/ServiceApiSiswa.kt
@@ -0,0 +1,14 @@
+package com.example.myapi.apiservice
+
+import com.example.myapi.modeldata.DataSiswa
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface ServiceApiSiswa {
+
+
+
+
+}
