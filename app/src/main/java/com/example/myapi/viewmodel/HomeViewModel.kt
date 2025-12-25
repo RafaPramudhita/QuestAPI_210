b4d9fe1a51a1041446727335015221ae9dd37037
RafaPramudhita <rafapram360@gmail.com>
2025-12-25 12:13:03 +0700

    Create StatusUiSiswa sealed interface

---
+++ app/src/main/java/com/example/myapi/viewmodel/HomeViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.myapi.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.myapi.modeldata.DataSiswa
+import com.example.myapi.repositori.RepositoriDataSiswa
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
+
+sealed interface StatusUiSiswa {
+    data class Success(
+        val siswa: List<DataSiswa> = listOf()
+    ) : StatusUiSiswa
+
+    object Error : StatusUiSiswa
+    object Loading : StatusUiSiswa
+}
+
