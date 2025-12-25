9f7d623b173d51f2e3c5b18397f88a7ac0f101f3
RafaPramudhita <rafapram360@gmail.com>
2025-12-25 12:03:05 +0700

    Create RepositoriDataSiswa interface

---
+++ app/src/main/java/com/example/myapi/repositori/RepositoryDataSiswa.kt
@@ -0,0 +1,11 @@
+package com.example.myapi.repositori
+
+import com.example.myapi.apiservice.ServiceApiSiswa
+import com.example.myapi.modeldata.DataSiswa
+import retrofit2.Response
+
+interface RepositoriDataSiswa {
+
+
+}
+
