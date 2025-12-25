d6a043bd40f3ca3ff922baedc7205e88603145c1
RafaPramudhita <rafapram360@gmail.com>
2025-12-25 12:13:49 +0700

    Handle loading and error state

--- app/src/main/java/com/example/myapi/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/myapi/viewmodel/HomeViewModel.kt
@@ -24,4 +24,25 @@ class HomeViewModel(
     private val repositoryDataSiswa: RepositoriDataSiswa
 ) : ViewModel() {
 
+    var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
+        private set
+
+    init {
+        loadSiswa()
+    }
+
+    fun loadSiswa() {
+        viewModelScope.launch {
+            listSiswa = StatusUiSiswa.Loading
+            listSiswa = try {
+                StatusUiSiswa.Success(
+                    repositoryDataSiswa.getDataSiswa()
+                )
+            } catch (e: IOException) {
+                StatusUiSiswa.Error
+            } catch (e: HttpException) {
+                StatusUiSiswa.Error
+            }
+        }
+    }
 }
