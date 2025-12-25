8afbc8b91de337dd7b7129889ca77be29c05e350
RafaPramudhita <rafapram360@gmail.com>
2025-12-25 12:07:56 +0700

    Setup navigation graph with NavHost

---
+++ app/src/main/java/com/example/myapi/uicontroller/PetaNavigasi.kt
@@ -0,0 +1,54 @@
+package com.example.myapi.uicontroller
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.myapi.uicontroller.route.DestinasiEntry
+import com.example.myapi.uicontroller.route.DestinasiHome
+import com.example.myapi.view.EntrySiswaScreen
+import com.example.myapi.view.HomeScreen
+
+@Composable
+fun DataSiswaApp(
+    navController: NavHostController = rememberNavController(),
+    modifier: Modifier
+) {
+    HostNavigasi(navController = navController)
+}
+
+@Composable
+fun HostNavigasi(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = modifier
+    ) {
+
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiEntry.route)
+                },
+                navigateToItemUpdate = {
+                    // nanti kalau ada detail/edit
+                }
+            )
+        }
+
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(
+                navigateBack = {
+                    navController.popBackStack()
+                }
+            )
+        }
+    }
+}
