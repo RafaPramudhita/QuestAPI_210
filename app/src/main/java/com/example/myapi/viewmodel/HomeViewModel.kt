47bed9a650d473e3d4e856411c2f1c2e17e12600
RafaPramudhita <rafapram360@gmail.com>
2026-01-05 01:11:34 +0700

    Refactor HomeViewModel formatting and streamline StatusUiSiswa interface

--- app/src/main/java/com/example/myapi/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/myapi/viewmodel/HomeViewModel.kt
@@ -12,10 +12,7 @@ import retrofit2.HttpException
 import java.io.IOException
 
 sealed interface StatusUiSiswa {
-    data class Success(
-        val siswa: List<DataSiswa> = listOf()
-    ) : StatusUiSiswa
-
+    data class Success(val siswa: List<DataSiswa> = listOf()) : StatusUiSiswa
     object Error : StatusUiSiswa
     object Loading : StatusUiSiswa
 }
@@ -27,17 +24,13 @@ class HomeViewModel(
     var listSiswa: StatusUiSiswa by mutableStateOf(StatusUiSiswa.Loading)
         private set
 
-    init {
-        loadSiswa()
-    }
+    init { loadSiswa() }
 
     fun loadSiswa() {
         viewModelScope.launch {
             listSiswa = StatusUiSiswa.Loading
             listSiswa = try {
-                StatusUiSiswa.Success(
-                    repositoryDataSiswa.getDataSiswa()
-                )
+                StatusUiSiswa.Success(repositoryDataSiswa.getDataSiswa())
             } catch (e: IOException) {
                 StatusUiSiswa.Error
             } catch (e: HttpException) {
