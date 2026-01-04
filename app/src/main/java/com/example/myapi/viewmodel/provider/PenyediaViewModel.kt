591dcd52d873ac1c4ade7831d4a9fa9f850bafa6
RafaPramudhita <rafapram360@gmail.com>
2026-01-05 01:11:20 +0700

    Add whitespace in PetaNavigasi.kt

--- app/src/main/java/com/example/myapi/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/myapi/viewmodel/provider/PenyediaViewModel.kt
@@ -1,28 +1,41 @@
 package com.example.myapi.viewmodel.provider
 
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.myapi.repositori.AplikasiDataSiswa
-import com.example.myapi.viewmodel.EntryViewModel
-import com.example.myapi.viewmodel.HomeViewModel
+import com.example.myapi.viewmodel.*
 
-fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
-        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
-                AplikasiDataSiswa
-        )
+fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa =
+    this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
 
 object PenyediaViewModel {
+
     val Factory = viewModelFactory {
+
         initializer {
-            HomeViewModel(aplikasiDataSiswa().container
-                .repositoryDataSiswa)
+            HomeViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
         }
+
         initializer {
-            EntryViewModel(aplikasiDataSiswa().container
-                .repositoryDataSiswa)
+            EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+
+        initializer {
+            DetailViewModel(
+                savedStateHandle = createSavedStateHandle(),
+                repository = aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+
+        initializer {
+            UpdateViewModel(
+                savedStateHandle = createSavedStateHandle(),
+                repository = aplikasiDataSiswa().container.repositoryDataSiswa
+            )
         }
     }
 }
