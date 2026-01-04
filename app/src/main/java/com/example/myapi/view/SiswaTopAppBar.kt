f89a533e6ea3b4740b63ec18ad6df0c654beb907
RafaPramudhita <rafapram360@gmail.com>
2026-01-05 01:08:41 +0700

    Refactor SiswaTopAppBar.kt code formatting and imports

--- app/src/main/java/com/example/myapi/view/SiswaTopAppBar.kt
+++ app/src/main/java/com/example/myapi/view/SiswaTopAppBar.kt
@@ -2,13 +2,7 @@ package com.example.myapi.view
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarScrollBehavior
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -23,7 +17,8 @@ fun SiswaTopAppBar(
     scrollBehavior: TopAppBarScrollBehavior? = null,
     navigateUp: () -> Unit = {}
 ) {
-    CenterAlignedTopAppBar(title = { Text(title) },
+    CenterAlignedTopAppBar(
+        title = { Text(title) },
         modifier = modifier,
         scrollBehavior = scrollBehavior,
         navigationIcon = {
@@ -35,5 +30,6 @@ fun SiswaTopAppBar(
                     )
                 }
             }
-        })
+        }
+    )
 }
